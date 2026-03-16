5eccc4c2cc0e0778a1b87e8857af1978abb454b0
CaffeinatedOpe <caffeinatedope@proton.me>
2026-03-16 10:03:54 -0500

    some changes

--- nix/vps.nix
+++ nix/vps.nix
@@ -1,25 +1,30 @@
 { config, pkgs, lib, inputs, ... }:
-
+let
+  unstable = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/master)
+    # reuse the current configuration
+    { config = config.nixpkgs.config; };
+in
 {
-  networking.hostName = "heimdall";
   environment.systemPackages = with pkgs; [
-    headscale
+    unstable.headscale
   ];
-	 networking.firewall = {
+  networking.hostName = "heimdall";
+  networking.firewall = {
     checkReversePath = "loose";
     trustedInterfaces = [ "tailscale0" ];
     allowedUDPPorts = [ config.services.tailscale.port ];
   };
 
   services = {
-		tailscale.enable = true;
+    tailscale.enable = true;
     headscale = {
       enable = true;
       address = "0.0.0.0";
-			port = 8080;
-			server_url = "https://headscale.caffeinatedope.net:443";
+      port = 8080;
       settings = {
-				baseDomain = "caffeinatedope.net";
+        server_url = "https://headscale.caffeinatedope.net:443";
+        dns.base_domain = "caffeinatedope.service";
         logtail.enabled = false;
         derp.enabled = true;
       };
