e90720d11620278526acfda70cc2f3beeb27867f
CaffeinatedOpe <caffeinatedope@proton.me>
2026-03-16 12:29:28 -0500

    headscale maybe?

--- nix/heimdall/heimdall.nix
+++ nix/heimdall/heimdall.nix
@@ -1,25 +1,28 @@
 { config, pkgs, lib, inputs, ... }: {
   networking.firewall.enable = false;
-  environment.systemPackages = with pkgs; [ headscale ];
+  environment.systemPackages = with pkgs; [ headscale docker-compose ];
   networking.hostName = "heimdall";
 
   environment.etc = {
-    docker-configs = {
-      source = ../hosts/heimdall/docker-configs;
-    };
+    docker-configs = { source = ../../hosts/heimdall/docker-configs; };
   };
 
   virtualisation.docker = {
-  enable = true;
-  # Set up resource limits
-  daemon.settings = {
-    experimental = true;
-    default-address-pools = [
-      {
+    enable = true;
+    # Set up resource limits
+    daemon.settings = {
+      experimental = true;
+      default-address-pools = [{
         base = "172.30.0.0/16";
         size = 24;
-      }
-    ];
+      }];
+    };
   };
-};
+  systemd.services.my-docker-compose = {
+    script = ''
+      docker-compose -f ${../../hosts/heimdall/docker-compose/security.yaml}
+    '';
+    wantedBy = [ "multi-user.target" ];
+    after = ["docker.service" "docker.socket"];
+  };
 }
