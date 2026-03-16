6acd15946e4ded76deb62bc81fe3bc5a62ffd829
CaffeinatedOpe <caffeinatedope@proton.me>
2026-03-16 11:49:57 -0500

    will this fix things

--- nix/vps.nix
+++ nix/vps.nix
@@ -1,6 +1,5 @@
-{ config, pkgs, lib, inputs, ... }:
-{
-	networking.firewall.enable = false;
+{ config, pkgs, lib, inputs, ... }: {
+  networking.firewall.enable = false;
   environment.systemPackages = with pkgs; [ headscale ];
   networking.hostName = "heimdall";
   networking.firewall = {
@@ -9,18 +8,10 @@
     allowedUDPPorts = [ config.services.tailscale.port ];
   };
 
-  services = {
-    tailscale.enable = true;
-    headscale = {
-      enable = true;
-      address = "0.0.0.0";
-      port = 8080;
-      settings = {
-        server_url = "https://headscale.caffeinatedope.net:443";
-        dns.base_domain = "caffeinatedope.service";
-        logtail.enabled = false;
-        derp.enabled = true;
-      };
+  environment.etc = {
+    etc-files = {
+      source = "/home/lucy/projects/infrastructure-as-code/nix/heimdall/config.yaml";
+      mode = "0440";
     };
   };
 }
