cad3d6edfd2d9fb1a7230c8540cb8ebf57b02c1a
CaffeinatedOpe <caffeinatedope@proton.me>
2026-03-16 10:18:16 -0500

    fix hash

--- nix/vps.nix
+++ nix/vps.nix
@@ -1,8 +1,8 @@
 { config, pkgs, lib, inputs, ... }:
 let
   unstable = import (builtins.fetchTarball {
-    url = "https://github.com/nixos/nixpkgs/tarball/master";
-    sha256 = "";
+    url = "https://github.com/nixos/nixpkgs/tarball/81b871c9b50f3c7115d78a45af0b42d7406abc81";
+    sha256 = "sha256:1vdxly2cjcq35ih1mc9idc8bjk6qn3n499hkmfk6d6anhszmnc18";
   })
   # reuse the current configuration
     { config = config.nixpkgs.config; };
